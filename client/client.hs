a6b87c6fa6312e78f384466e6eb20f953c874928
Jonathan Skårstedt <jonathan.skarstedt@gmail.com>
2015-02-02 09:54:22 +0100

    mapping out client functionality

---
+++ client/client.hs
@@ -0,0 +1,26 @@
+
+import Crypto.Scrypt 
+import Data.Map
+
+data Avatar = ImageB64
+            | GravatarMail String
+            | ImageURL URI
+--            | Server stored/encrypted binary image? 
+            | None
+
+newtype User
+  = User (PrivKey, PubKey) Avatar Name
+
+-- Locally generate a Public Key pair
+generateKeyPair :: RandomGen g =>
+                   g -> Salt -> (PrivKey, PubKey)
+generateKeyPair = undefined
+
+-- Encrypt a message using a public key
+encrypt :: Text -> PubKey -> Text
+encrypt = undefined
+
+-- Decrypt a message using a private key
+decrypt :: Text -> PrivKey -> Text
+decrypt = undefined
+
